3fcc46866f5e62aa9feebb9a2656a87832a294f2
YD <yuriidiachuk@icloud.com>
2024-11-22 23:49:11 +0200

    update searchForm

--- src/pages/MoviesPage/Movies.jsx
+++ src/pages/MoviesPage/Movies.jsx
@@ -1,13 +1,24 @@
 import { Field, Form, Formik } from "formik";
+import MovieList from "../../components/MovieList/MovieList";
+import { useState } from "react";
+import { fetchMoviesByQuery } from "../../services/api";
 
 const MoviesPage = () => {
-  const handleSubmit = (values, options) => {
-    console.log(values);
+  const [movies, setMovies] = useState([]);
+  const [query, setQuery] = useState("");
+  const handleSubmit = async (values, options) => {
+    const results = await fetchMoviesByQuery(values.query);
+    setMovies(results);
+    setQuery(values.query);
     options.resetForm();
   };
   const initialValues = {
     query: "",
   };
+  const filteredMovies = movies.filter((movie) =>
+    movie.title.toLowerCase().includes(query.toLowerCase())
+  );
+
   return (
     <div>
       <Formik onSubmit={handleSubmit} initialValues={initialValues}>
@@ -16,6 +27,7 @@ const MoviesPage = () => {
           <button type="submit">Search</button>
         </Form>
       </Formik>
+      <MovieList movies={filteredMovies} />
     </div>
   );
 };
